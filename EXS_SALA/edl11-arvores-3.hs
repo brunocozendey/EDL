d2e559345a073c8f7453c570c8273b5dd1f587ce
brunocozendey <brunocozendey@gmail.com>
2019-10-06 21:35:56 -0300

    ainda precisa corrigir o possui 2 pois n~ao para qundo encontra x maior que y
    Signed-off-by: brunocozendey <brunocozendey@gmail.com>

--- EXS_SALA/edl11-arvores-3.hs
+++ EXS_SALA/edl11-arvores-3.hs
@@ -10,12 +10,24 @@ dobra (Folha x)     = Folha (2*x)
 dobra (Galho a1 a2) = Galho (dobra a1) (dobra a2)
 
 possui :: Arvore -> Int -> Bool
-possui (Folha x) y = y == x
-possui (Galho a1 a2) y = possui a1 y || possui a2 y
+possui (Folha x) y      = y == x
+possui (Galho a1 a2) y  = possui a1 y || possui a2 y
 
-a0 = Galho a1 (Folha 1)
+possui2 :: Arvore -> Int -> Bool
+possui2 (Folha x) y       =  if y < x then False else x == y --else (possui2 a1) y || (possui2 a2) y
+possui2 (Galho a1 a2) y   = possui2 a1 y || possui2 a2 y  
+
+--maximo :: Arvore -> Int
+--maximo Folha            = 0
+--maximo (Galho x a1 a2)  = maximo a2
+
+--insere :: Arvore -> Int -> Arvore
+--insere Folha x              = Folha
+--insere (Galho x a1 a2) y    = x == y || if y > x then () 
+
+a0 = Galho (Folha 1) a1 
 a1 = Galho a2 a3
 a2 = Galho (Folha 6) (Folha 7)
-a3 = Galho (Folha 8) (Folha 9)
+a3 = Galho (Folha 2) (Folha 9)
 
-main = print $ (a0, soma a0, dobra a0, possui a1 2, possui a1 9)
+main = print $ (a0, soma a0, dobra a0, possui a0 2, possui a0 9, possui2 a0 2)
